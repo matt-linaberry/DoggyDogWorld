bf951dbd9de409437b82e2399c4875a1ed7f264f
Matt Linaberry <mtlinaberry@gmail.com>
2015-12-08 17:20:31 -0500

    Thru Lesson 5-8
    This is the initial push for this project…

---
+++ app/src/main/java/edu/self/doggydogworld/Dog.java
@@ -0,0 +1,11 @@
+package edu.self.doggydogworld;
+
+/**
+ * Created by mlinaberry on 12/8/15.
+ */
+public class Dog {
+    String name;
+    int age;
+    int numberOfLegs = 4;
+    String breed;
+}
